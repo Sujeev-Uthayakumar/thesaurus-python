f49ff31ff3e175a1a7409d18b2d37c76005a0993
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-08-21 10:20:39 -0400

    main

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 def loadJSON():
     data = json.load(open("data.json"))
@@ -9,8 +10,16 @@ def find(key):
     key = key.lower()
     if key in data:
         return output(data[key])
+    elif len(get_close_matches(key, data.keys())) > 0:
+        key = get_close_matches(key, data.keys(),n=1)[0]
+        print("Did you mean {} instead".format(key))
+        user = input("Would you like to try this {}? Y/N ".format(key))
+        if user == 'y' or 'Y':
+            return output(data[key])
+        else:
+            return print("This word does not exist. Please double check it.")
     else:
-        return print("This word does not exist")
+        return print("This word does not exist. Please double check it.")
 
 def output(definition):
     print(definition)

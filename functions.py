30384bf8b6c694bbab1e71031c212d70f0483cf0
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-08-21 10:41:10 -0400

    main

--- functions.py
+++ functions.py
@@ -14,13 +14,19 @@ def find(key):
         key = get_close_matches(key, data.keys(),n=1)[0]
         print("Did you mean {} instead".format(key))
         user = input("Would you like to try this {}? Y/N ".format(key))
-        if user == 'y' or 'Y':
+        if user == 'Y' or user == 'y':
             return output(data[key])
-        else:
+        elif user == 'N' or user == 'n':
             return print("This word does not exist. Please double check it.")
+        else:
+            return print("The entry cannot be understood!")
     else:
         return print("This word does not exist. Please double check it.")
 
-def output(definition):
-    print(definition)
+def output(definitions):
+    count = 0
+    print("Definitions:")
+    for definition in definitions:
+        count = count + 1
+        print("{0}: {1}".format(count, definition))
 
